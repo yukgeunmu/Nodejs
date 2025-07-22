d8c1f6021ab3ecd858a5f3cb5c6cc671d8207d61
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-07-22 17:00:44 +0900

    📖 Study:"인증 인가 구현"

---
+++ COMUNITY_HUB/src/middlewares/auth.middleware.js
@@ -0,0 +1,50 @@
+import jwt from "jsonwebtoken";
+import { prisma } from "../utils/prisma/index.js";
+
+// 1. 클라이언트로 부터 **쿠키(Cookie)**를 전달받습니다.
+// 2. **쿠키(Cookie)**가 **Bearer 토큰** 형식인지 확인합니다.
+// 3. 서버에서 발급한 **JWT가 맞는지 검증**합니다.
+// 4. JWT의 `userId`를 이용해 사용자를 조회합니다.
+// 5. `req.user` 에 조회된 사용자 정보를 할당합니다.
+// 6. 다음 미들웨어를 실행합니다.
+
+export default async function (req, res, next) {
+  try {
+    const { authorization } = req.cookies;
+    
+    if (!authorization) throw new Error("토큰이 존재하지 앟습니다.");
+
+    const [tokenType, token]= authorization.split(' ');
+
+    if (tokenType !== "Bearer") throw Error("토큰 타입이 일치하지 않습니다.");
+
+    const decodedToken = jwt.verify(token, "custom-secret-key");
+    const userId = decodedToken.userId;
+
+    const user = await prisma.users.findFirst({
+      where: { userId: +userId },
+    });
+
+    if (!user) {
+      res.clearCookie("authorization");
+      throw Error("토크 사용자가 존재하지 않습니다.");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.clearCookie("authorization");
+
+    // 토큰이 만료되었거나, 조작되었을 때, 에러 메시지를 다르게 출력합니다.
+    switch (error.name) {
+      case "TokenExpiredError":
+        return res.status(401).json({ message: "토큰이 만료되었습니다." });
+      case "JsonWebTokenError":
+        return res.status(401).json({ message: "토큰이 조작되었습니다." });
+      default:
+        return res
+          .status(401)
+          .json({ message: error.message ?? "비정상적인 요청입니다." });
+    }
+  }
+}
---
+++ COMUNITY_HUB/src/routes/users.router.js
@@ -0,0 +1,109 @@
+import express from "express";
+import { prisma } from "../utils/prisma/index.js";
+import bcrpyt from "bcrypt";
+import jwt from "jsonwebtoken";
+import authMiddleware from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+// **[게시판 프로젝트] 회원가입 API 비즈니스 로직**
+
+// 1. `email`, `password`, `name`, `age`, `gender`, `profileImage`를 **body**로 전달받습니다.
+// 2. 동일한 `email`을 가진 사용자가 있는지 확인합니다.
+// 3. **Users** 테이블에 `email`, `password`를 이용해 사용자를 생성합니다.
+// 4. **UserInfos** 테이블에 `name`, `age`, `gender`, `profileImage`를 이용해 사용자 정보를 생성합니다.
+
+router.post("/sign-up", async (req, res, next) => {
+  const { email, password, name, age, gender, profileImaage } = req.body;
+
+  const isExistEmail = await prisma.users.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (isExistEmail) {
+    return res.status(409).json({ message: "이미 존재하는 이메일 입니다." });
+  }
+
+  const hashedPassword = await bcrpyt.hash(password, 10); // 사용자 비밀번호를 암호화
+
+  const user = await prisma.users.create({
+    data: {
+      email,
+      password: hashedPassword,
+    }, // 암호화된 비밀번호를 저장
+  });
+
+  const userInfo = await prisma.userInfos.create({
+    data: {
+      userId: user.userId,
+      name,
+      age,
+      gender: gender.toUpperCase(),
+      profileImaage,
+    },
+  });
+
+  return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+});
+
+// 1. `email`, `password`를 **body**로 전달받습니다.
+// 2. 전달 받은 `email`에 해당하는 사용자가 있는지 확인합니다.
+// 3. 전달 받은 `password`와 데이터베이스의 저장된 `password`를 bcrypt를 이용해 검증합니다.
+// 4. 로그인에 성공한다면, 사용자에게 JWT를 발급합니다.
+
+router.post("/sign-in", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await prisma.users.findFirst({ where: { email } });
+
+  if (!user)
+    return res.status(409).json({ message: "존재하지 않는 이메일 입니다." });
+
+  if (!(await bcrpyt.compare(password, user.password)))
+    return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
+
+  const token = jwt.sign(
+    {
+      userId: user.userId,
+    },
+    "custom-secret-key"
+  );
+
+  res.cookie('authorization', `Bearer ${token}`);
+  return res.status(200).json({message: '로그인 성공'});
+
+});
+
+
+// src/routes/users.route.js
+
+/** 사용자 조회 API **/
+router.get('/users', authMiddleware, async (req, res, next) => {
+  const { userId } = req.user;
+
+  const user = await prisma.users.findFirst({
+    where: { userId: +userId },
+    select: {
+      userId: true,
+      email: true,
+      createdAt: true,
+      updatedAt: true,
+      userInfos: {
+        // 1:1 관계를 맺고있는 UserInfos 테이블을 조회합니다.
+        select: {
+          name: true,
+          age: true,
+          gender: true,
+          profileImage: true,
+        },
+      },
+    },
+  });
+
+  return res.status(200).json({ data: user });
+});
+
+
+
+export default router;
